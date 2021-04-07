295594ea92795d11e89192b3cb51c9ab8c9b0009
josh <og.ginger@gmail.com>
2021-04-07 16:03:13 -0700

    Created project 'asyncArrayMethods: Asynchronous implementations for primitive array methods.

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import Test from "./lib/test"
+import * as asyncArrayMethods from "./asyncArrayMethods"
+
+async function main() {
+    console.log("main.ts initialized successfully!");
+}
+
+main();
